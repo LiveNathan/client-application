117d5a347c84e83c4e526727386e7d82833d8733
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-04 15:42:54 -0600

    endpoint is working

---
+++ src/test/java/cnLabs/Micro/clientapplication/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cnLabs.Micro.clientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
